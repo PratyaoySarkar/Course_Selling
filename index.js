a28fc92237590168091c8161546f81910ed69103
Pratyaoy Sarkar <sarkarpratyaoy@gmail.com>
2025-01-19 22:57:57 +0530

    Added admin router

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const {courseRouter} = require("./routes/course");
 const {userRouter} = require("./routes/users");
+const {admitRouter} = require("./routes/admin");
 const app = express();
 
-app.use("/user", userRouter);
+app.use("/user", userRouter); //Divide the routes into different files
 app.use("/course", courseRouter);
+app.use("/admin", admitRouter);
 
 //createUserRouter(app);
 //createCourseRouter(app);
---
+++ routes/admin.js
@@ -0,0 +1,38 @@
+const { Router } = require("express");
+const adminRouter = Router(); //The Router is the function not a class
+
+adminRouter.post("/signup", function (req, res) {
+    res.json({
+        message: "signup endpoint"
+    })
+})
+
+adminRouter.get("/signin", function (req, res) {
+    res.json({
+        message: "signup endpoint"
+    })
+})
+
+adminRouter.use(adminMiddleware); //Middleware for admin after signup or signin
+
+adminRouter.post("/create-course", function (req, res) {
+    res.json({
+        message: "signup endpoint"
+    })
+})
+
+adminRouter.put("/update-course", function (req, res) {
+    res.json({
+        message: "signup endpoint"
+    })
+})
+
+adminRouter.get("/course/bulk", function (req, res) {
+    res.json({
+        message: "signup endpoint"
+    })
+})
+
+module.exports = {
+    adminRouter: adminRouter
+}
